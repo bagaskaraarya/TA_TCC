5782238d1c006ccd89df2ad1d21a0f42612b713c
Arya Bhagaskara <bagaskaraarya213@gmail.com>
2025-06-03 12:41:43 +0700

    fe fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bulma/css/bulma.css";
+import axios from "axios";
+import React from "react";
+
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
